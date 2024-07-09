b02ad8529d16d3fee8b375ae9ca479e09b59d7d4
Ahmed jaziri <ahmed.archimatch@gmail.com>
2024-07-09 12:37:49 +0200

    feat(login_form): first screen interface

--- src/app/(supplier)/(public)/login.tsx
+++ src/app/(supplier)/(public)/login.tsx
@@ -1,21 +1,14 @@
-import { useRouter } from 'expo-router';
 import React from 'react';
 
-import { useAuth } from '@/core';
 import { useSoftKeyboardEffect } from '@/core/keyboard';
-import type { LoginFormProps } from '@/modules/login/login-form';
-import { LoginForm } from '@/modules/login/login-form';
+import type { LoginFormProps } from '@/modules/login/login-form-supplier';
+import { LoginForm } from '@/modules/login/login-form-supplier';
 import { FocusAwareStatusBar } from '@/shared/components';
 
 export default function Login() {
-  const router = useRouter();
-  const signIn = useAuth.use.signIn();
   useSoftKeyboardEffect();
 
-  const onSubmit: LoginFormProps['onSubmit'] = () => {
-    signIn({ access: 'access-token', refresh: 'refresh-token' });
-    router.push('/(supplier)/(private)');
-  };
+  const onSubmit: LoginFormProps['onSubmit'] = () => {};
   return (
     <>
       <FocusAwareStatusBar />
